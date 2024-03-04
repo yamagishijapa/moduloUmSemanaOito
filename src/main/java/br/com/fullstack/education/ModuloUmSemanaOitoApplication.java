bb2990491ec1f86aa27ce3ef3db5539cd3e7d438
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-03-04 12:31:25 -0300

    [M1S08] Ex 4 - Service: Alunos  - Implementada classe AlnuoService;  - Montada execução de teste na Main.

--- src/main/java/br/com/fullstack/education/ModuloUmSemanaOitoApplication.java
+++ src/main/java/br/com/fullstack/education/ModuloUmSemanaOitoApplication.java
@@ -1,13 +1,26 @@
 package br.com.fullstack.education;
 
+import br.com.fullstack.education.model.Aluno;
+import br.com.fullstack.education.services.AlunoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ModuloUmSemanaOitoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuloUmSemanaOitoApplication.class, args);
+		AlunoService alunoService = new AlunoService();
+
+		alunoService.cadastrarAluno("Thiago", LocalDate.of(1997, 11, 16));
+		alunoService.cadastrarAluno("Maria Julia", LocalDate.of(1997, 5, 30));
+
+		System.out.println("Alunos Cadastrados:");
+		for (Aluno aluno : alunoService.consultarAlunos()) {
+			System.out.println(aluno.toString());
+		}
 	}
 
 }
